06f2bc991f9b5970db5c2a0a6f81fabef95f0c9d
Mahdi <48333830+mahdi-mk@users.noreply.github.com>
2022-09-07 04:03:40 +0300

    Update HomePageController.php

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -2,18 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Question;
-use Illuminate\Http\Request;
-
 class HomePageController extends Controller
 {
     public function __invoke()
     {
-        $topQuestions = Question::query()
-            ->paginate(20);
-
-        return inertia('Home', [
-            'topQuestions' => $topQuestions
-        ]);
+        return inertia('Home');
     }
 }
